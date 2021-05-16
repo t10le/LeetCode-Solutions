6d90433fc5636f77ddcb34c77bf378ebcac5ac7f
t10le <tom.tommy.le@gmail.com>
2021-05-16 15:19:22 -0400

    improved 1544

--- Python/make-the-string-great.py
+++ Python/make-the-string-great.py
@@ -3,9 +3,7 @@ from typing import *
 
 class Solution:
     def makeGood(self, s: str) -> str:
-        # Remove all pairs of adjacent upperLowers
-        pair = lambda x, y: x != y
-
+        """Returns the 'good' copy of the given string, 's', which is a string that does not have two adjacent characters that are equal in alphabetical value and the pair contains an upper and lower-case character."""
         # -- Even --
         # aABb --> ""
         # abBA --> ""
@@ -18,10 +16,26 @@ class Solution:
         # aBaAbc --> 'ac'
         # aBc --> 'aBc'
 
+        # -- Submission Stats (as of May 16, 2021) --
+        # Time Complexity: O(n)
+        # Runtime: 28ms, faster than 95.34%
+        # Space Complexity: O(n)
+        # Memory Usage: 14.3 MB, less than 49.29%
+
         if len(s) == 1:
             return s
 
-        result = ""
-        for i in range(len(s) - 1):
-            if s[i] != s[i+1]:
+        # treat result like a stack
+        result = [s[0]]
 
+        for i in range(1, len(s)):
+            # if the 'result' list is not empty, compare elements and indices
+            if result:
+                if result[-1].lower() == s[i].lower() and result[-1] != s[i]:
+                    result.pop(-1)
+                else:
+                    result.append(s[i])
+            else:
+                result.append(s[i])
+
+        return "".join(e for e in result)
