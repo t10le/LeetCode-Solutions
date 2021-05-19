209fc9f4996323af4d352abebb3d0a68df3a23d0
t10le <tom.tommy.le@gmail.com>
2021-05-19 10:00:23 -0400

    improved 0094

--- Python/binary-tree-inorder-traversal.py
+++ Python/binary-tree-inorder-traversal.py
@@ -16,10 +16,41 @@ class Solution:
         # Runtime: 24ms, faster than 94.51%
         # Space Complexity: O(n)
         # Memory Usage: 14.3 MB, less than 12.23%
+
         # if root == None:
         #     return []
         # else:
         #     return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
 
-        # NEW SOLUTION
+        # ALTERNATIVE SOLUTION
+        # -- Submission Stats (as of May 17, 2021) --
+        # Time Complexity: O(n)
+        # Runtime: 28ms, faster than 80.67%
+        # Space Complexity: O(n)
+        # Memory Usage: 14 MB, less than 98.49%
+
+        stack, result, visited = [root], [], {root}
+        if root == None:
+            return []
+
+        while stack:
+            node = stack.pop()
+            skip = True
+
+            if node.right and node.right not in visited:
+                stack.append(node.right)
+                visited.add(node.right)
+                skip = False
+
+            stack.append(node)
+
+            if node.left and node.left not in visited:
+                stack.append(node.left)
+                visited.add(node.left)
+                skip = False
+
+            if skip:
+                result.append(stack.pop().val)
+
+        return result
 
