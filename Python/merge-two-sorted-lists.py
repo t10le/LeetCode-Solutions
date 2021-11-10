04b6f1b42d0f6c22f2b8768e747eb5a6e19388f8
t10le <tom.tommy.le@gmail.com>
2021-11-09 23:17:36 -0500

    partially done 21

---
+++ Python/merge-two-sorted-lists.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # base case
+
+        # recursive case
+        self.mergeTwoLists(l1.next, l2.next)
