172ccf82ef1c0d46a1fee7ed41e7231031cd559e
t10le <tom.tommy.le@gmail.com>
2021-11-10 23:52:56 -0500

    partial 21

--- Python/merge-two-sorted-lists.py
+++ Python/merge-two-sorted-lists.py
@@ -5,7 +5,15 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # base case
-
-        # recursive case
-        self.mergeTwoLists(l1.next, l2.next)
+        q = []
+        while l1 or l2:
+            if l1:
+                q.append(l1)
+                l1 = l1.next
+            if l2:
+                q.append(l2)
+                l2 = l2.next
+        q.sort(key=lambda x: x.val)
+        for i in range(len(q)-1):
+            q[i].next = q[i+1]
+        return q[0] if len(q) else None
